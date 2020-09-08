04c0d315eca8c8597434f2ef2d503f136c638e93
luislortega <a18016323@alumnos.uady.mx>
2020-09-08 11:40:33 -0500

    Nuevos scripts agregados: 1.52.0

---
+++ ScriptMapas.lua
@@ -0,0 +1,97 @@
+PACK DE CODIGOS PARA EL MAPA [MODIFICACIONES VISULAES DEL MAPA]
+
+AMPLIAR CAMARA
+EDITABLE = "EDITABLE"
+function A()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 36506244
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("7F 00 44 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 36506244
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("70 4C 2D E9 10 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+MODO NOCHE
+EDITABLE = "EDITABLE"
+function B()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libunity.so")
+    myoffset = 1712768
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 80 BF")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libunity.so")
+    myoffset = 1712768
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("BD 37 86 35 00 20 A0 E3")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+REMOVER PASTO
+EDITABLE = "EDITABLE"
+function C()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 27129176
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 27129176
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("F0 4F 2D E9 1C B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+MAPA HD
+EDITABLE = "EDITABLE"
+function D()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 43899768
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0  E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 43899768
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("30 48 2D E9 08 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
---
+++ ScriptsPVP.lua
@@ -0,0 +1,465 @@
+PACK CODIGOS PVP [ARMAS Y MODIFICACIONESD PARA COMBATE]
+
+RESALTAR MIRA
+EDITABLE = "EDITABLE"
+function A()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("00r;00r;00r;3Fr;00r;00r;80r;3Fr;00r;00r;00r;00r;00r;00r;00r;00r;00r;00r;00r;3Fr::20", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(20)
+    gg.editAll("00r;00r;A0r;40r;00r;00r;80r;3Fr;00r;00r;00r;00r;00r;00r;00r;00r;00r;00r;A0r;40r", gg.TYPE_BYTE)
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    gg.searchNumber("00r;00r;A0r;40r;00r;00r;80r;3Fr;00r;00r;00r;00r;00r;00r;00r;00r;00r;00r;A0r;40r::20", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(20)
+    gg.editAll("00r;00r;00r;3Fr;00r;00r;80r;3Fr;00r;00r;00r;00r;00r;00r;00r;00r;00r;00r;00r;3Fr", gg.TYPE_BYTE)
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+SEMI APUNTADO
+EDITABLE = "EDITABLE"
+function B()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12393420
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("5C 04 44 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12393420
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("60 00 90 E5 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+AIMLOCK
+EDITABLE = "EDITABLE"
+function C()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12471852
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("0F 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12471852
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("3C 01 90 E5 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+MIRA FIJA
+EDITABLE = "EDITABLE"
+function D()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12329708
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_C_DATA)
+    gg.searchNumber("00h;00h;A0h;E3h;1Eh;FFh;2Fh;E1h;04h;D0h;4Dh;E2h::12", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(8)
+    gg.editAll("F0h;4Fh;2Dh;E9h;1Ch;B0h;8Dh;E2h", gg.TYPE_BYTE)
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+DISPARAR CORRIENDO
+EDITABLE = "EDITABLE"
+function E()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 10763248
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("01 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 10763248
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("70 4C 2D E9 10 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+CAMBIO DE ARMA RAPIDO
+EDITABLE = "EDITABLE"
+function F()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 10828612
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 10828612
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("F0 48 2D E9 10 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+RECARGAR RAPIDO
+EDITABLE = "EDITABLE"
+function G()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12395144
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("12 03 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12395144
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("70 4C 2D E9 10 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+LLUVIA DE PUÑOS
+EDITABLE = "EDITABLE"
+function H()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12394428
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("8A 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12394428
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("50 00 90 E5 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+QUITAR RECOIL DE ARMAS
+EDITABLE = "EDITABLE"
+function I()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 11082716
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("03 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 11082716
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("F0 4D 2D E9 18 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+REMOVER LAG
+EDITABLE = "EDITABLE"
+function J()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12448748
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("12 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12448748
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("F0 4F 2D E9 1C B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+DISPARO DOBLE
+EDITABLE = "EDITABLE"
+function K()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12405076
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12405076
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("70 4C 2D E9 10 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+REMOVER MIRA
+EDITABLE = "EDITABLE"
+function L()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 10792948
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 10792948
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("70 4C 2D E9 10 B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+AIMHEADSHOT
+function M()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 28208240
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("10 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 28208908
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("10 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 28209204
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("10 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("96r;00r;00r;00r;00r;00r;B0r;40r;00r;00r;80r;3Fr;00r;00r;40r;3Fr::16", gg.TYPE_BYTE)
+  gg.getResults(16)
+  gg.editAll("96r;00r;00r;00r;00r;00r;B8r;40r;00r;00r;00r;00r;00r;00r;00r;00r", gg.TYPE_BYTE)
+  gg.clearResults()
+  gg.toast("EDITABLE")
+end
+
+LLUVIA DE BALAS
+function N()
+  chuv = gg.multiChoice({
+    "🔴 ᴅᴇsᴀᴄᴛɪᴠᴀʀ 🔴",
+    "🚿 ʟʟᴜᴠɪᴀ ᴅᴇ ʙᴀʟᴀs x10 🚿",
+    "🚿 ʟʟᴜᴠɪᴀ ᴅᴇ ʙᴀʟᴀs x20 🚿",
+    "🚿 ʟʟᴜᴠɪᴀ ᴅᴇ ʙᴀʟᴀs x30 🚿",
+    "↩️"
+  }, nil, "🚿 ʟʟᴜᴠɪᴀ ᴅᴇ ʙᴀʟᴀs - ʜᴀᴄᴋᴇʀᴏ 2.0🚿")
+  if chuv == nil then
+  else
+    if chuv[1] == true then
+      chuvaoff()
+    end
+    if chuv[2] == true then
+      chuva10()
+    end
+    if chuv[3] == true then
+      chuva20()
+    end
+    if chuv[4] == true then
+      chuva30()
+    end
+    if chuv[5] == true then
+      START()
+    end
+  end
+  XGCK = -1
+end
+function chuvaoff()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 12395128
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("00 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.toast("𝗔𝗖𝗧𝗜𝗩𝗔𝗗𝗢 🔴")
+end
+function chuva10()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 12395128
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("10 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.toast("𝗔𝗖𝗧𝗜𝗩𝗔𝗗𝗢 🔵")
+end
+function chuva20()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 12395128
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("20 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.toast("𝗔𝗖𝗧𝗜𝗩𝗔𝗗𝗢 🔵")
+end
+function chuva30()
+  gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+  name("libil2cpp.so")
+  myoffset = 12395128
+  original("7F 45 4C 46 01 01 01 00")
+  replaced("30 00 A0 E3 1E FF 2F E1")
+  gg.clearResults()
+  gg.toast("𝗔𝗖𝗧𝗜𝗩𝗔𝗗𝗢 🔵")
+end
+
+DISPARO RAPIDO
+EDITABLE = "EDITABLE"
+function Ñ()
+  if EDITABLE == "EDITABLE" then
+    gg.clearResults()
+    gg.setRanges(gg.REGION_CODE_APP)
+    name("libil2cpp.so")
+    myoffset = 12395040
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("02 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12395040
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("5C 00 90 E5 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+FANTASMA
+EDITABLE = "EDITABLE"
+function A()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12544740
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("00 00 A0 E3 1E FF 2F E1")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libil2cpp.so")
+    myoffset = 12544740
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("F0 4F 2D E9 1C B0 8D E2")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
+
+PIEDRA
+EDITABLE = "EDITABLE"
+function B()
+  if EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libunity.so")
+    myoffset = 15673488
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("C9 3C 8E BF C9 3C 8E BF")
+    gg.clearResults()
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libunity.so")
+    myoffset = 15673496
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("C9 3C 8E BF C9 3C 8E BF")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  elseif EDITABLE == "EDITABLE" then
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libunity.so")
+    myoffset = 15673488
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("BD 37 86 35 BD 37 86 35")
+    gg.clearResults()
+    gg.setRanges(gg.REGION_CODE_APP | gg.REGION_C_DATA)
+    name("libunity.so")
+    myoffset = 15673496
+    original("7F 45 4C 46 01 01 01 00")
+    replaced("BD 37 86 35 BD 37 86 35")
+    gg.clearResults()
+    gg.toast("EDITABLE")
+    EDITABLE = "EDITABLE"
+  end
+end
